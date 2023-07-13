854842390e9c7292402afcf95d2786044223df38
Ward Childress <ward.childress@gmail.com>
2023-07-13 15:25:09 -0400

    Added a Contains test to hTSprite.
    Added a Contains test to hTSprite to check if an x and y fall within its dimensions. Made some comments clearer on world space and scale.

--- hosTile/hTSprite.h
+++ hosTile/hTSprite.h
@@ -26,18 +26,22 @@ namespace hosTile
 		DirectX::XMFLOAT3 GetPosition() const;
 		void SetPosition(DirectX::XMFLOAT3 position);
 
-		unsigned int GetWidth() const;		// in pixels
-		unsigned int GetHeight() const;		// in pixels
+		unsigned int GetWidth() const;		// unscaled, in pixels
+		unsigned int GetHeight() const;		// unscaled, in pixels
 
 		float GetScale() const;
 		void SetScale(float scale);
 		void SetXFlip(bool xFlip);
 		void SetYFlip(bool yFlip);
 
+		// Are x and y are contained within the sprite, in world space?
+		bool Contains(float x, float y) const;
+
 	protected:
 		// Swaps the values of two UVs. Handy for applying x-flip, y-flip, etc.
 		void SwapUVs(DirectX::XMFLOAT2& uv1, DirectX::XMFLOAT2& uv2) const;
 
+		// Position is the midpoint of the sprite in world space.
 		DirectX::XMFLOAT3 m_position;
 
 		unsigned int m_width;		// in pixels
